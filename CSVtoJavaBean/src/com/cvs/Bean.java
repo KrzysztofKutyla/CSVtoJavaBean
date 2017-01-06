27277815fce0a34b85abb3b1bd0d078c37a7ccdd
Krzysztof <krzysztof.kutyla@fastmail.fm>
2017-01-06 19:27:15 +0100

    Second

--- CSVtoJavaBean/src/com/cvs/Bean.java
+++ CSVtoJavaBean/src/com/cvs/Bean.java
@@ -3,8 +3,8 @@ package com.cvs;
 public class Bean {
 
 	int id;
-	String imiê;
-	String nazwisko;
+	String name;
+	String surname;
 	String email;
 	String gender;
 	String ip_adress;
@@ -12,15 +12,13 @@ public class Bean {
 	String country;
 	String language;
 	String description;
-	
-	
-	
-	public Bean(int id, String imiê, String nazwisko, String email, String gender, String ip_adress,
+
+	public Bean(int id, String name, String surname, String email, String gender, String ip_adress,
 			String company_name, String country, String language, String description) {
 		super();
 		this.id = id;
-		this.imiê = imiê;
-		this.nazwisko = nazwisko;
+		this.name = name;
+		this.surname = surname;
 		this.email = email;
 		this.gender = gender;
 		this.ip_adress = ip_adress;
@@ -29,79 +27,91 @@ public class Bean {
 		this.language = language;
 		this.description = description;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
-		return "Bean [id=" + id + ", imiê=" + imiê + ", nazwisko=" + nazwisko + ", email=" + email + ", gender="
+		return "Bean [id=" + id + ", imiï¿½=" + name + ", surname=" + surname + ", email=" + email + ", gender="
 				+ gender + ", ip_adress=" + ip_adress + ", company_name=" + company_name + ", country=" + country
 				+ ", language=" + language + ", description=" + description + "]";
 	}
 
-
-
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getImiê() {
-		return imiê;
+
+	public String getname() {
+		return name;
 	}
-	public void setImiê(String imiê) {
-		this.imiê = imiê;
+
+	public void setname(String name) {
+		this.name = name;
 	}
-	public String getNazwisko() {
-		return nazwisko;
+
+	public String getsurname() {
+		return surname;
 	}
-	public void setNazwisko(String nazwisko) {
-		this.nazwisko = nazwisko;
+
+	public void setsurname(String surname) {
+		this.surname = surname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getIp_adress() {
 		return ip_adress;
 	}
+
 	public void setIp_adress(String ip_adress) {
 		this.ip_adress = ip_adress;
 	}
+
 	public String getCompany_name() {
 		return company_name;
 	}
+
 	public void setCompany_name(String company_name) {
 		this.company_name = company_name;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getLanguage() {
 		return language;
 	}
+
 	public void setLanguage(String language) {
 		this.language = language;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
-	
 }
